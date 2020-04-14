242855421c2c9d875dc37b87945fdbdef4cfe28a
Francisco Miguel <miguelbh6@gmail.com>
2020-04-14 18:03:25 -0300

    formatacao de codigo

--- application/models/Pessoa_model.php
+++ application/models/Pessoa_model.php
@@ -1,20 +1,22 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pessoa_model extends MY_Model {
+class Pessoa_model extends MY_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTabela('pessoa');
+    }
 
-	function __construct() {
-		parent::__construct();
-		$this->setTabela('pessoa');
-	}
+    public function getByNome()
+    {
+        return $this->db->order_by('nome', 'ASC')->get($this->tabela)->result();
+    }
 
-	public function getByNome()
-	{
-		return $this->db->order_by('nome', 'ASC')->get($this->tabela)->result();	
-	}
-
-	public function getByCpf($id) {
+    public function getByCpf($id)
+    {
         return $this->db->where('cpf', $id)->get($this->tabela)->row();
     }
 }
--- application/views/fragments/alert.php
+++ application/views/fragments/alert.php
@@ -1,20 +1,20 @@
 <div class="row">
 	<div class="col-md-12">
-		<?php if ($this->session->flashdata('msg-sucess')) : ?>
+		<?php if ($this->session->flashdata('msg-sucess')): ?>
 			<div class="alert alert-success alert-dismissible fade show" role="alert">
-				<?= $this->session->flashdata('msg-sucess'); ?>
+				<?=$this->session->flashdata('msg-sucess');?>
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
-		<?php endif; ?>
-		<?php if ($this->session->flashdata('msg-error')) : ?>
+		<?php endif;?>
+		<?php if ($this->session->flashdata('msg-error')): ?>
 			<div class="alert alert-danger alert-dismissible fade show" role="alert">
-				<?= $this->session->flashdata('msg-error'); ?>
+				<?=$this->session->flashdata('msg-error');?>
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
-		<?php endif; ?>
+		<?php endif;?>
 	</div>
 </div>
