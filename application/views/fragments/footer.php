fc7a910f52025c5965ee964856e54b67dc24b3bd
Francisco Miguel <miguelbh6@gmail.com>
2020-04-14 17:44:50 -0300

    Modularização

---
+++ application/views/fragments/footer.php
@@ -0,0 +1,12 @@
+<footer>
+<div class="row">
+<div class="col-12">
+<nav class="navbar relative-bottom navbar-expand-sm navbar-dark bg-dark">
+<a class="navbar-brand" href="#"> <img
+src="<?=base_url();?>assets/img/favicon.ico" width="30"
+height="30" class="d-inline-block align-top" alt=""> CNDN
+</a>
+</nav>
+</div>
+</div>
+</footer>
--- application/views/fragments/template.php
+++ application/views/fragments/template.php
@@ -1,37 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<?php require_once(__DIR__ . '../../fragments/head.php'); ?>
+<?php require_once __DIR__ . 'head.php';?>
 
 <body>
-    <div class="row">
-        <div class="col-12">
-            <nav class="navbar navbar-dark bg-dark">
-                <a class="navbar-brand" href="<?=base_url(); ?>"> <img
-                        src="<?=base_url(); ?>assets/img/favicon.ico" width="30" height="30"
-                        class="d-inline-block align-top" alt=""> CNDN
-                </a>
-            </nav>
-        </div>
-    </div>
+    <?php require_once 'nav.php';?>
     <br>
     <div class="container">
-        <?php require_once(__DIR__ . '../../fragments/alert.php'); ?>
-        <?=$contents; ?>
+        <?php require_once 'alert.php';?>
+        <?=$contents;?>
     </div>
-
-    <!--<footer>
-	<div class="row">
-	<div class="col-12">
-		<nav class="navbar relative-bottom navbar-expand-sm navbar-dark bg-dark">
-			<a class="navbar-brand" href="#"> <img
-				src="<?=base_url(); ?>assets/img/favicon.ico" width="30"
-				height="30" class="d-inline-block align-top" alt=""> CNDN
-			</a>
-		</nav>
-	</div>
-</div>
-<?php require_once(__DIR__ . '../../fragments/footer.php'); ?>
-</footer>-->
+    <?php require_once 'footer.php';?>
 </body>
-
 </html>
