89619a6ce66341d04e81a42a27b893582f07a448
Francisco Miguel <miguelbh6@gmail.com>
2020-04-16 14:44:05 -0300

    Remocao de index.html

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-#$config['base_url'] = 'http://localhost/cadrestitui';
-$config['base_url'] = 'http://cndn.com.br/cadrestitui';
+$config['base_url'] = 'http://localhost/cadrestitui';
+#$config['base_url'] = 'http://cndn.com.br/cadrestitui';
 
 /*
 |--------------------------------------------------------------------------
