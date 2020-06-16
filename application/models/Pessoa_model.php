38e7a8ed816007475d46cb6be0ffda29b87daecf
Francisco Jr <miguelbh6@gmail.com>
2020-06-16 18:00:15 -0300

    Alterações solicitadas em 15/06/2020

--- application/models/Pessoa_model.php
+++ application/models/Pessoa_model.php
@@ -27,10 +27,10 @@ class Pessoa_model extends MY_Model
 
     public function obterComDadosBancarios()
     {
-        $this->db->select('p.id as id, p.nome, p.cpf, b.nome as banco, pb.agencia, pb.conta, pb.vl_total, pb.cpf, pb.tpconta');
+        $this->db->select('p.id as id, p.nome, p.cpf, b.nome as banco, pb.agencia, pb.conta, pb.vl_total, pb.tpconta');
         $this->db->from('pessoa p');
-        $this->db->join('pessoabanco pb', 'p.cpf = pb.cpf', 'inner');
-        $this->db->join('banco b', 'b.id = pb.banco', 'inner');
+        $this->db->join('pessoabanco pb', 'p.cpf = pb.cpf', 'left');
+        $this->db->join('banco b', 'b.id = pb.banco', 'left');
         $this->db->order_by('p.nome', 'asc');
         return $this->db->get()->result();
     }
