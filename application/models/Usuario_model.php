53957320e00c8653de3eff78371bded58d776702
Francisco Miguel <miguelbh6@gmail.com>
2020-10-16 15:41:08 -0300

    Alteração em login, count atualizado em versao do php 7.2 ou superior

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -5,8 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Usuario_model extends CI_Model {
 
     function existeUsuario($pUsuario, $pSenha) {
+        $this->db->from('usuario');
         $this->db->where('usuario', $pUsuario);
         $this->db->where('senha', md5($pSenha));
-        return count($this->db->get('usuario')) > 0;
+        return $this->db->count_all_results() > 0;
     }
 }
